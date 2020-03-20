fb6bf36b1583420f44080d0e0880071679b8a237
bartek <bartekperucki@gmail.com>
2020-03-20 10:34:12 +0100

    Add back to about page

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const About = () => {
   return (
     <div className="about">
+      <div className="about__back">
+        <Link to="/"><span>{"<"}</span> Back</Link>
+      </div>
       <h1 className="about__app-title">Rate that Bridge!</h1>
 
       <div className="about__content">
